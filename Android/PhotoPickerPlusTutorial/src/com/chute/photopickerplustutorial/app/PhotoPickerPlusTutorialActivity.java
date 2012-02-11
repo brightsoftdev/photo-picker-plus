64ad371a345f1a38f9eb03aa5b983c3cace14b64
Olga Lazarova <olahla@gmail.com>
2012-02-11 21:27:41 +0100

    ImageView added for loading the selected image

--- Android/PhotoPickerPlusTutorial/src/com/chute/photopickerplustutorial/app/PhotoPickerPlusTutorialActivity.java
+++ Android/PhotoPickerPlusTutorial/src/com/chute/photopickerplustutorial/app/PhotoPickerPlusTutorialActivity.java
@@ -5,6 +5,7 @@ import com.chute.photopickerplustutorial.R;
 import com.chute.photopickerplustutorial.R.id;
 import com.chute.photopickerplustutorial.R.layout;
 import com.chute.photopickerplustutorial.intent.PhotoPickerPlusIntentWrapper;
+import com.darko.imagedownloader.ImageLoader;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -12,17 +13,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class PhotoPickerPlusTutorialActivity extends Activity {
 
     public static final String TAG = PhotoPickerPlusTutorialActivity.class.getSimpleName();
+	private ImageView image;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.photo_picker_plus_activity);
 
-	findViewById(R.id.btnChoose).setOnClickListener(new OnChooseClickListener());
+	findViewById(R.id.btnPhotoPicker).setOnClickListener(new OnChooseClickListener());
+	image = (ImageView) findViewById(R.id.imageView);
+	
     }
 
     private class OnChooseClickListener implements OnClickListener {
@@ -40,6 +45,7 @@ public class PhotoPickerPlusTutorialActivity extends Activity {
 	    return;
 	}
 	final PhotoActivityIntentWrapper wrapper = new PhotoActivityIntentWrapper(data);
+	ImageLoader.get(this).displayImage(wrapper.getMediaModel().getUrl(), image);
 	Log.d(TAG, wrapper.toString());
     }
 }
